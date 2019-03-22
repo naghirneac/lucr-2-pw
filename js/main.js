c8542c08d4be2b460665ed4e030378b4bd30917c
Naghirneac Ana <33444068+naghirneac@users.noreply.github.com>
2019-03-22 12:26:01 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Naghirneac Luvrare2
 window.onload = function () {
 
     //ex 1
@@ -20,9 +21,39 @@ window.onload = function () {
             document.getElementById('container').classList.add('verde');
         })
     //ex 4
-    for (var i = 1; i <= 10; i++){
-        if (document.getElementsByTagName('p').i % 2 == 0) document.getElementsByTagName('p').style.color = "blue"
-            else document.getElementsByTagName('p').style.color = "yellow";
+    for (var i = 1 ; i <= 10; i++){
+        if (i % 2 == 0) document.getElementsByTagName('p').style.backgroundColor = "blue";
+            else document.getElementsByTagName('p').style.backgroundColor = "yellow";
+    }
+    //ex 5
+    var list = document.createElement('ul');
+    document.body.appendChild(lista);
+    for (var i = 1 ; i <= 10; i++){
+        var li = document.createElement('li');
+        list.appendChild(li.textContent = "Element Nr "+i++);
+    }
+    //ex 6
+    list.id = "lista";
+    document.body.appendChild(list);
+    document.querySelector('#lista li:last-child').style.backgroundColor = "violet";
+    //ex 8
+    function createCerc(){
+        for(var i = 1; i <= 10; i++){
+            var cerc = document.createElement("div");
+            cerc.classList.add('cerc');
+            document.getElementById('idcerc').appendChild(cerc);
+        }}
+   
+    //ex 10
+    setTimeout(function schimbCuloarea() {
+        document.body.style.backgroundColor = "silver";
+    }, 5000);
 
+    //ex 12
+    function contor(){
+        var n = 1;
+        document.getElementById('contor').addEventListener('click' ,  function(){
+            document.getElementById('contor').innerHTML = "Contor " + n++);
+        }
     }
-    }
+
